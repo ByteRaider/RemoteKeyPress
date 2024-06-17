fa7f424d392a2b3a864d0ba002c8a37be909e065
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-06-17 11:47:44 -0700

    Add ApplicationListSerializer

--- backend/keypress/serializers.py
+++ backend/keypress/serializers.py
@@ -3,3 +3,6 @@ from rest_framework import serializers
 class CheckboxActionSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     action = serializers.ChoiceField(choices=['check', 'uncheck'])
+
+class ApplicationListSerializer(serializers.Serializer):
+    windows = serializers.ListField(child=serializers.CharField(max_length=255))
