c81f06efa5f925e5c28c10bbffb0afbf885e0266
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-06-18 21:54:58 -0700

    Update urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from keypress.urls import urlpatterns as keypress_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')), # browsable API rest framework
-    path('keypress/', include('keypress.urls')), # Checkbox and Key Press API
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')), # browsable API rest framework
+    path('keypress/', include('keypress.urls', namespace='keypress')), # Checkbox and Key Press API
 ]
