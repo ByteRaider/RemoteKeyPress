722e5359c8dc3e2e33db02427479252831ddec88
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-06-21 17:09:39 -0700

    Update urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -7,6 +7,6 @@ from gui_navigator.urls import urlpatterns as gui_navigator_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')), # browsable API rest framework
-    path('keypress/', include('keypress.urls', namespace='keypress')), # Checkbox and Key Press API
+    path('keypress/', include('keypress.urls', namespace='keypress')), # 
     path('guinav/', include('gui_navigator.urls', namespace='guinav')),
 ]
